c639456130d85853d2c2409c9ad414f1cee07e61
Júlia Martins Monteiro <juliamamonteiro02@gmail.com>
2026-05-28 23:34:07 +0000

    new file:   classe.py 	new file:   teste.py

---
+++ classe.py
@@ -0,0 +1,17 @@
+class GerenciamentoNotas:
+    def calcular_media(n1, n2):
+        media = (n1 + n2)/2 
+        return 
+
+    def validar_nota(media):
+        if media >= 7:
+            print("foi aprovado")
+        else if ( 5 >= media <= 6.9):
+            print("está em recuperação")
+        else: 
+            print("Está reprovado")
+
+
+            
+
+        
---
+++ teste.py
@@ -0,0 +1,15 @@
+import classe.py
+
+n1 = float(input("nota 1"))
+n2 = float(input("nota 2"))
+
+naosei = classe.GerenciamentoNotas()
+
+
+
+
+
+
+
+
+    
